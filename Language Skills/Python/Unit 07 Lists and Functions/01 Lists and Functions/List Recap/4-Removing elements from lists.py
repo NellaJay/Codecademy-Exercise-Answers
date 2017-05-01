da4bf66bf936d0beff62b03e24b44c05d4e9e0ef
NellaJay <kathryn.rader1998@gmail.com>
2017-05-01 13:35:55 -0400

    Update 4-Removing elements from lists.py
    While I was working on code academy, I tried the previous code that was there before I changed it and it was simply delete the 1 in the 1, 3, 5. If this is different on other computers please consider putting a disclaimer for BOTH problems. Thank you! :)

--- Language Skills/Python/Unit 07 Lists and Functions/01 Lists and Functions/List Recap/4-Removing elements from lists.py
+++ Language Skills/Python/Unit 07 Lists and Functions/01 Lists and Functions/List Recap/4-Removing elements from lists.py
@@ -1,9 +1,4 @@
-n = [1, 3, 5]
-# Removes 1 from the list,
-# NOT the item at index 1
-n.remove(1)
-# Another possible solution, will remove the item at the given index:
-del(n[0])
-# Another possible solution will remove the item at index from the list and return it to you:
-n.pop(0)
+n = [3, 5]
+# Remove the first item in the list here
+
 print n
